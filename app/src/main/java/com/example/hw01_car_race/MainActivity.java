ac178510b9ae969fdcbc514d0611d31ed6bc5ae2
shlomi2522 <shlomi2591996@gmail.com>
2021-10-30 16:05:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hw01_car_race/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.hw01_car_race;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
